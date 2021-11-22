04f781e0add265ec5c6c9e629ddc4cc61a337d2b
PatricEdwards <46666525+PatricEdwards@users.noreply.github.com>
2021-11-22 12:09:33 +0200

    Add files via upload

---
+++ InsertStatements.sql
@@ -0,0 +1,14 @@
+INSERT INTO vehicle (Name, Model,Price)
+VALUES ('Polo Vivo', 'Trendline',  227900.00);
+
+INSERT INTO vehicle (Name, Model,Price)
+VALUES ('Polo Vivo', 'Comfortline',   246000.00);
+
+INSERT INTO vehicle (Name, Model,Price)
+VALUES ('Polo Vivo', 'Highline',   272800.00);
+
+INSERT INTO vehicle (Name, Model,Price)
+VALUES ('Polo Sedan', 'Trendline',   272800.00);
+
+INSERT INTO vehicle (Name, Model,Price)
+VALUES ('Polo Sedan', 'Comfortline',   281300.00);
